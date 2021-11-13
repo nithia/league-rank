0cfafbd96f1296cb11d2cc939e3943a574e594aa
Nithia Govender <nithia@mindworks.software>
2021-11-13 17:06:41 +0200

    Tests for score parsing

---
+++ cmd/ranking.go
@@ -0,0 +1,16 @@
+package main
+
+type score struct {
+	Team  string
+	Goals int
+}
+
+type result [2]score
+
+func main() {
+
+}
+
+func parseScore(input string) score {
+	return score{}
+}
---
+++ cmd/ranking_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseScore(t *testing.T) {
+	input := "Lions 3"
+
+	parsed := parseScore(input)
+
+	expected := score{
+		Team:  "Lions",
+		Goals: 3,
+	}
+
+	assert.Equal(t, expected, parsed)
+}
+
+func TestParseScoreWithMultipleSpaces(t *testing.T) {
+	input := "Lions   2"
+
+	parsed := parseScore(input)
+
+	expected := score{
+		Team:  "Lions",
+		Goals: 2,
+	}
+
+	assert.Equal(t, expected, parsed)
+}
+
+func TestParseScoreWithSpaceInTeam(t *testing.T) {
+	input := "Nelson Mandela Bay Lions 0"
+
+	parsed := parseScore(input)
+
+	expected := score{
+		Team:  "Lions",
+		Goals: 0,
+	}
+
+	assert.Equal(t, expected, parsed)
+}
