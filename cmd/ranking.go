35406629fb449690893487471e8e2a20c60805af
Nithia Govender <nithia@mindworks.software>
2021-11-14 19:33:43 +0200

    Refactor: move data structures into separate package

--- cmd/ranking.go
+++ cmd/ranking.go
@@ -1,43 +1,5 @@
 package main
 
-import (
-	"strconv"
-	"strings"
-)
-
-type score struct {
-	Team  string
-	Goals int
-}
-
-type result []score
-
 func main() {
 
-}
-
-func parseResult(line string) result {
-	splitScores := strings.Split(line, ",")
-
-	res := result{}
-	for _, splitScore := range splitScores {
-		res = append(res, parseScore(strings.TrimSpace(splitScore)))
-	}
-
-	return res
-}
-
-func parseScore(input string) score {
-	lastSpace := strings.LastIndex(input, " ")
-
-	return score{
-		Team:  strings.TrimSpace(input[:lastSpace]),
-		Goals: parseInt(input[lastSpace+1:]),
-	}
-}
-
-func parseInt(s string) int {
-	i, _ := strconv.Atoi(s) // Ignore error as input is guaranteed to be well-formed
-
-	return i
 }
---
+++ pkg/league/league.go
@@ -0,0 +1,39 @@
+package league
+
+import (
+	"strconv"
+	"strings"
+)
+
+type score struct {
+	Team  string
+	Goals int
+}
+
+type result []score
+
+func parseResult(line string) result {
+	splitScores := strings.Split(line, ",")
+
+	res := result{}
+	for _, splitScore := range splitScores {
+		res = append(res, parseScore(strings.TrimSpace(splitScore)))
+	}
+
+	return res
+}
+
+func parseScore(input string) score {
+	lastSpace := strings.LastIndex(input, " ")
+
+	return score{
+		Team:  strings.TrimSpace(input[:lastSpace]),
+		Goals: parseInt(input[lastSpace+1:]),
+	}
+}
+
+func parseInt(s string) int {
+	i, _ := strconv.Atoi(s) // Ignore error as input is guaranteed to be well-formed
+
+	return i
+}
